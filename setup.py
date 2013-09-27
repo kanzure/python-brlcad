f05ecf78de2773d10e388213aeace737c48184b1
Bryan Bishop <kanzure@gmail.com>
2013-09-26 22:57:49 -0500

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,21 +1,56 @@
-import distutils.core
-from distutils.command.install import install as Installer
+from setuptools import setup
+from setuptools.command.install import install as InstallCommand
+from setuptools.command.develop import develop as DevelopCommand
+
+def pre_install():
+    print "PRE_INSTALL"
 
 def post_install():
-    """
-    After regular installation is complete, this function acts as a
-    post-install script to generate the bindings against whatever version of
-    BRL-CAD is installed.
-    """
-    import ctypesgen.ctypesgencore as ctypesgencore
-    print "ctypesgen version: " + str(ctypesgencore.__version__)
+    print "POST_INSTALL"
 
-class CustomInstaller(Installer):
-    def run(self):
-        Installer.run(self)
-        post_install()
+def setuptoolcommandify(command_subclass):
+    """
+    A decorator for subclasses of setuptool commands that calls a pre_run hook
+    and a post_run hook.
+    """
+    original_run = command_subclass.run
 
-distutils.core.setup(
+    def modified_run(self):
+        """
+        Call the original run implementation, but wrap it with pre-run and
+        post-run hooks.
+        """
+        self.pre_run()
+        original_run(self)
+        self.post_run()
+
+    # attach the new implementation
+    command_subclass.run = modified_run
+
+    return command_subclass
+
+class CustomCommandMixin(object):
+    """
+    Just a way to get the hooks into the subclasses in a DRY manner. Override
+    these defaults if something more specific has to happen in a particular
+    command.
+    """
+
+    def pre_run(self):
+        return pre_install()
+
+    def post_run(self):
+        return post_install()
+
+@setuptoolcommandify
+class CustomDevelopCommand(DevelopCommand, CustomCommandMixin):
+    pass
+
+@setuptoolcommandify
+class CustomInstallCommand(InstallCommand, CustomCommandMixin):
+    pass
+
+setup(
     name="thing_postinstall",
     version="0.0.1",
     description="An attempt at a post-install script.",
@@ -24,6 +59,7 @@ distutils.core.setup(
     url="https://github.com/kanzure/python-brlcad",
     packages=["thing_postinstall"],
     cmdclass={
-        "install": CustomInstaller,
+        "install": CustomInstallCommand,
+        "develop": CustomDevelopCommand,
     },
 )
