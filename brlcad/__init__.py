ab0db31fa69b19fe00775559e9ee6e92bd66e09c
Bryan Bishop <kanzure@gmail.com>
2013-10-26 21:20:25 -0500

    clarify brlcad/__init__.py use of __all__

--- brlcad/__init__.py
+++ brlcad/__init__.py
@@ -4,5 +4,6 @@ __all__ = []
 
 _path_bindings = os.path.join(os.path.dirname(__file__), "_bindings")
 
+# The generated bindings might not exist yet and importing will fail.
 if os.path.exists(_path_bindings):
     __all__.append("_bindings")
