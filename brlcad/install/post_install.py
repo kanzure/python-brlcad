d7d4d4620be3a8961c95126b8c2c68a6c451b212
Csaba Nagy <ncsaba@javampire.com>
2013-12-26 10:49:51 +0100

    Fix library dependencies

--- brlcad/install/post_install.py
+++ brlcad/install/post_install.py
@@ -224,9 +224,9 @@ def main(library_path, logger=None):
         Make up the dependencies to each library so that ctypesgen will be able
         to import the relevant structures from each module.
         """
-        brlcad_libraries["wdb"]["dependencies"] += ["bu", "bn", ]
-        brlcad_libraries["rt"]["dependencies"] += ["bu", "bn", "wdb", ]
-        brlcad_libraries["ged"]["dependencies"] += ["bu", "bn", "wdb", "rt", ]
+        brlcad_libraries["rt"]["dependencies"]  += ["bu", "bn", ]
+        brlcad_libraries["wdb"]["dependencies"] += ["bu", "bn", "rt" ]
+        brlcad_libraries["ged"]["dependencies"] += ["bu", "bn", "rt", "wdb", ]
 
         # not sure about the capitalization on this one
         #brlcad_libraries["brep"]["dependencies"].append("openNURBS")
