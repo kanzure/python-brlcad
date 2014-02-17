f46f0bbfd51d5f93cc7ea41bc220594a13e93dd6
Csaba Nagy <ncsaba@javampire.com>
2014-02-17 22:46:57 +0100

    Replace 'from ... import *' with explicit list

--- brlcad/primitives/__init__.py
+++ brlcad/primitives/__init__.py
@@ -2,13 +2,13 @@
 Python wrappers for the BRL-CAD primitives.
 """
 
-from base import *
-from arb8 import *
-from arbn import *
-from combination import *
-from ellipsoid import *
-from rpc import *
-from tgc import *
+from base import Primitive
+from arb8 import ARB8
+from arbn import ARBN
+from combination import Combination, negate, intersect, subtract, union, xor, wrap_tree
+from ellipsoid import Ellipsoid, Sphere
+from rpc import RPC
+from tgc import TGC, Cone, RCC, TRC
 
 __all__ = [
     "Primitive", "ARB8", "ARBN", "Ellipsoid", "Sphere", "RPC", "TGC", "Cone", "RCC", "TRC",
--- brlcad/vmath/__init__.py
+++ brlcad/vmath/__init__.py
@@ -4,9 +4,9 @@ Not all macros are translated to python versions, only those which
 are needed for the higher level operations implemented in python.
 """
 
-from plane import *
-from segment import *
-from transform import *
-from vector import *
+from plane import Plane
+from segment import Segment
+from transform import Transform
+from vector import Vector, X, Y, Z
 
 __all__ = ["Vector", "Segment", "Plane", "Transform", "X", "Y", "Z"]
