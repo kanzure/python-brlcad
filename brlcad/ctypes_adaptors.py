50d29e89171a95cdfafad51aee1d90435de46b20
kraj.reddy@lnmiit.ac.in <mohit.daga@ieee.org>
2014-06-02 23:37:58 +0530

    Add a different class of 2d-Array where cols are fixed

--- brlcad/ctypes_adaptors.py
+++ brlcad/ctypes_adaptors.py
@@ -142,6 +142,15 @@ def array2d_from_pointer(t, num_rows, num_cols):
     return np.array(result)
 
 
+def array2d_fixed_cols(t, num_cols_fixed=5, data_type=ctypes.c_double):
+    arrays = []
+    for i in range(len(t)):
+        array = (data_type*num_cols_fixed)(*t[i])
+        arrays.append(brlcad_copy(array,"array2d_fixed_cols"))
+    result = ((data_type*num_cols_fixed)*len(t))(*arrays)
+    return ctypes.cast(brlcad_copy(result, "array2d_fixed_cols"), ctypes.POINTER(data_type*num_cols_fixed))
+
+
 def array2d(t, data_type=ctypes.c_double):
     arrays = []
     for i in range(len(t)):
